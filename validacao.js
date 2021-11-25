42443beac87c746b19cd15b3f450baa181b5bf3a
STEPHANE CRISTINE VILAS BOAS <boas.stephane@alunoescolassociais.org.br>
2021-11-25 17:08:41 -0300

    atualiza trabalho

--- validacao.js
+++ validacao.js
@@ -17,28 +17,25 @@ function verificar(){
 
 function maskFone(){
 
-var numeroAtual = document.getElementById('fone').value
+var numeroAtual = document.getElementById('fone').value;
 const isCelular = numeroAtual.length === 11;
 const isFone = numeroAtual.length === 10;
-
 let numeroAjustado;
 
 if(isCelular){
-
  const part1 = numeroAtual.slice(0,2);
  const part2 = numeroAtual.slice(2,7);
  const part3 = numeroAtual.slice(7,11);
-numeroAjustado = `{${part1})${part2}-${part3}}`
-
+numeroAjustado = `(${part1}) ${part2}-${part3}`
 } else if (isFone){
  const part1 = numeroAtual.slice(0,2);
  const part2 = numeroAtual.slice(2,6);
  const part3 = numeroAtual.slice(6,10);
-numeroAjustado = `{${part1})${part2}-${part3}}`
+numeroAjustado =  `(${part1}) ${part2}-${part3}`
 }else{
-numeroAtual.value='';
- alert("Número infrormado é incorreto, favor verificar!")	
+	numeroAtual.value='';
+ 	alert("Número infrormado é incorreto, favor verificar!")	
 }
- numeroAtual.value = numeroAjustado;
+ fone.value = numeroAjustado;
 
 }
